75d996208996e88fc61e9f7e03bfaf00b0647a54
Darryl <dazsnow@gmail.com>
2020-02-21 22:42:43 +0800

    add reload button on random facts

--- src/components/random-facts.tsx
+++ src/components/random-facts.tsx
@@ -6,6 +6,7 @@ const FactContainer = styled.div`
   align-items: center;
   display: flex;
   justify-content: center;
+  position: relative;
   opacity: 0;
   height: 100vh;
   width: 100vw;
@@ -14,31 +15,70 @@ const FactContainer = styled.div`
     opacity: 1;
     transition: 0.15s 0.45s opacity ease-out;
   }
+`
 
-  h2 {
-    color: var(--colors-background);
-    font-size: 3em;
-    text-align: center;
+const Button = styled.button`
+  background: none;
+  border: none;
+  box-shadow: none;
+  color: var(--colors-background);
+  cursor: pointer;
+  font-size: 2em;
+  font-weight: 600;
+  left: 50%;
+  opacity: 0.5;
+  outline: none;
+  position: absolute;
+  top: var(--spacing);
+  transition: opacity 0.15s ease-in-out;
+  transform: translateX(-50%);
 
-    @media (min-width: 55em) {
-      font-size: 4em;
-    }
+  &:active,
+  &:hover {
+    animation: 0.45s linear infinite rotate;
+    opacity: 1;
   }
 
-  a {
-    border-bottom: 1px solid var(--colors-highlight);
-    color: var(--colors-background);
-    font-weight: 600;
-    opacity: 0.75;
-    text-decoration: none;
-    transition: all 0.15s ease-in-out;
-
-    &:active,
-    &:focus,
-    &:hover {
-      border-bottom: 1px solid var(--colors-background);
-      opacity: 1;
+  @keyframes rotate {
+    from {
+      transform: translateX(-50%) rotate(0);
     }
+
+    to {
+      transform: translateX(-50%) rotate(1turn);
+    }
+  }
+`
+
+const Fact = styled.h2`
+  color: var(--colors-background);
+  font-size: 3em;
+  opacity: 1;
+  text-align: center;
+  transition: 0.15s opacity ease-in-out;
+
+  @media (min-width: 55em) {
+    font-size: 4em;
+  }
+
+  &.is-faded-out {
+    opacity: 0;
+  }
+`
+
+const Link = styled.a`
+  border-bottom: 1px solid var(--colors-highlight);
+  color: var(--colors-background);
+  font-weight: 600;
+  opacity: 0.75;
+  text-decoration: none;
+  transition: all 0.15s ease-in-out;
+
+  &:active,
+  &:focus,
+  &:hover {
+    border-bottom: 1px solid var(--colors-background);
+    opacity: 1;
   }
 `
 
@@ -53,139 +93,148 @@ const RandomFacts = (props: any): React.ReactElement => {
     switch (fact) {
       case 1:
         return (
-          <h2>
+          <>
             I like brewing beer
             <br />
             <Emoji text=":beer:" />
-          </h2>
+          </>
         )
         break
       case 2:
         return (
-          <h2>
+          <>
             I once delayed a Chinese military aircraft by accidentally bowling a sack of pomelos down the runway...
             <br />
             <Emoji text=":airplane:" />
-          </h2>
+          </>
         )
         break
       case 3:
         return (
-          <h2>
+          <>
             I once dressed up as a rabbit, walked into a supermarket, and bought all of the carrots.
             <br />
             <Emoji text=":rabbit:" />
-          </h2>
+          </>
         )
         break
       case 4:
         return (
-          <h2>
+          <>
             I was&nbsp;
-            <a
+            <Link
               href="http://content.time.com/time/specials/packages/0,28757,2019341,00.html"
               title="I'm pretty sure they were referring to me..."
             >
               Time Magazine Person of the Year in 2006
-            </a>
+            </Link>
             <br />
             <Emoji text=":medal:" />
-          </h2>
+          </>
         )
         break
       case 5:
         return (
-          <h2>
+          <>
             I like listening to&nbsp;
-            <a href="https://youtu.be/s36eQwgPNSE" title="Listen to my totally tubular playlist">
+            <Link href="https://youtu.be/s36eQwgPNSE" title="Listen to my totally tubular playlist">
               80's music
-            </a>
+            </Link>
             <br />
             <Emoji text=":sunglasses:" />
-          </h2>
+          </>
         )
         break
       case 6:
         return (
-          <h2>
+          <>
             I designed&nbsp;
-            <a href="https://en.wikipedia.org/wiki/Snow_(beer)" title="Guess how I got this gig...">
+            <Link href="https://en.wikipedia.org/wiki/Snow_(beer)" title="Guess how I got this gig...">
               the logo for Snow beer
-            </a>
+            </Link>
             <br />
             <small>
               <em>(the best-selling beer brand in the world)</em>
             </small>
             <br />
             <Emoji text=":beer:" />
-          </h2>
+          </>
         )
         break
       case 7:
         return (
-          <h2>
+          <>
             I like eating weird things...
             <br />
             <Emoji text=":brain:" />
-          </h2>
+          </>
         )
         break
       case 8:
         return (
-          <h2>
+          <>
             I like hot weather
             <br />
             <Emoji text=":sun:" />
-          </h2>
+          </>
         )
         break
       case 9:
         return (
-          <h2>
+          <>
             I like spicy food
             <br />
             <Emoji text=":spicy:" />
-          </h2>
+          </>
         )
         break
       case 10:
         return (
-          <h2>
+          <>
             I don't like reading fiction
             <br />
             <Emoji text=":book:" />
-          </h2>
+          </>
         )
         break
       case 11:
         return (
-          <h2>
+          <>
             I like swimming
             <br />
             <Emoji text=":swimmer:" />
-          </h2>
+          </>
         )
         break
       default:
         return (
-          <h2>
+          <>
             I used to be a&nbsp;
-            <a href="https://www.youtube.com/watch?v=8myh_7RrAck" title="Watch me in action!">
+            <Link href="https://www.youtube.com/watch?v=8myh_7RrAck" title="Watch me in action!">
               TV weatherman
-            </a>
+            </Link>
             <br />
             <Emoji text=":snowman:" />
-          </h2>
+          </>
         )
     }
   }
 
   const [fact, setFact] = React.useState(getRandomNumber())
+  const [isFaded, changeFade] = React.useState(false)
+
+  const getNewFact = () => {
+    changeFade(true)
+    setTimeout(() => {
+      changeFade(false)
+      setFact(getRandomNumber())
+    }, 150)
+  }
 
   return (
     <FactContainer>
-      <button onClick={() => setFact(getRandomNumber())}>new</button>
-      {renderFact()}
+      <Button onClick={getNewFact}>&#8635;</Button>
+      <Fact className={isFaded ? 'is-faded-out' : ''}>{renderFact()}</Fact>
     </FactContainer>
   )
 }
